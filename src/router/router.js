58c8350131a5328b310981d7bffca7e7a3b51dbf
zubeir <zubwd1@gmail.com>
2024-09-20 07:59:13 +0100

    corect route file

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
-import ShowCusmoter from '@/components/customer/ShowCusmoter.vue';
-import AddOrder from '@/components/order/AddOrder.vue';
-import ShowOrder from '@/components/order/ShowOrder.vue';
-import ShowProduct from '@/components/product/ShowProduct.vue';
+import ShowCusmoter from '../components/customer/showCusmoter.vue';
+import AddOrder from '../components/order/AddOrder.vue';
+import ShowOrder from '../components/order/ShowOrder.vue';
+import ShowProduct from '../components/product/ShowProduct.vue';
 
 
 export const routes = [
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
 });
