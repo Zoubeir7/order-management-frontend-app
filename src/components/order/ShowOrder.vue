9984f54de0061c0f6e8db3695375d65d8f14f75f
zubeir <zubwd1@gmail.com>
2024-09-20 01:40:02 +0100

    Add alias path to src folder

--- src/components/order/ShowOrder.vue
+++ src/components/order/ShowOrder.vue
@@ -41,7 +41,7 @@
 
         <div class="modal fade" id="orderDetailsModal" tabindex="-1" aria-labelledby="orderDetailsModalLabel"
             aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="orderDetailsModalLabel">View order</h5>
